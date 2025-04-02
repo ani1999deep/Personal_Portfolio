98596818c75a99c99d874b0525802aace0c406dd
Anideep1 <anideepbhowmick1@gmail.com>
2025-04-03 00:00:56 +0530

    Upto mobile view.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/sections/Home";
 import About from "./components/sections/About";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
+import { MobileMenu } from "./components/MobileMenu";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -20,7 +21,7 @@ function App() {
         className={`app-container ${isLoaded ? "app-visible" : "app-hidden"}`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
         <Projects />
