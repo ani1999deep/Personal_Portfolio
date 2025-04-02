1e89c8b992b8837d592c19e1988745692f11aa28
Anideep1 <anideepbhowmick1@gmail.com>
2025-04-02 23:52:43 +0530

    Upto Navbar seactions.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
 
-const App = () => {
+import "./index.css";
+import Home from "./components/sections/Home";
+import About from "./components/sections/About";
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+
+function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
-      <div>
-        <LoadingScreen onComplete={() => console.log("Loading complete!")} />
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`app-container ${isLoaded ? "app-visible" : "app-hidden"}`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
       </div>
     </>
   );
-};
+}
 
 export default App;
